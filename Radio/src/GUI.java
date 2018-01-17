b2657f7d96357657259c3426781fdf529f4124ea
LCEST <lcesturban10@gmail.com.com>
2018-01-16 20:57:07 -0600

    GUI Final

--- Radio/src/GUI.java
+++ Radio/src/GUI.java
@@ -41,13 +41,16 @@ public class GUI extends javax.swing.JFrame {
         jButton14 = new javax.swing.JButton();
         jButton15 = new javax.swing.JButton();
         jButton16 = new javax.swing.JButton();
+        jButton17 = new javax.swing.JButton();
+        jButton18 = new javax.swing.JButton();
+        jCheckBox1 = new javax.swing.JCheckBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jButton1.setText("ON");
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 70, 40));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 60, 40));
 
         jButton2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jButton2.setText("OF");
@@ -56,68 +59,79 @@ public class GUI extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 10, 70, 40));
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 10, 60, 40));
 
         jTextField1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jTextField1.setText("123.1");
         getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 10, 80, 40));
 
         jButton3.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton3.setText("<");
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 10, 70, 40));
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 10, 60, 40));
 
         jButton4.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton4.setText(">");
-        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 10, 70, 40));
+        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 10, 60, 40));
 
         jButton5.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton5.setText("1");
-        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 100, 60, -1));
+        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 140, 60, -1));
 
         jButton6.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton6.setText("2");
-        getContentPane().add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 100, 60, -1));
+        getContentPane().add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 140, 60, -1));
 
         jButton7.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton7.setText("3");
-        getContentPane().add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 100, 60, -1));
+        getContentPane().add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, 60, -1));
 
         jButton8.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton8.setText("4");
-        getContentPane().add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 100, 60, -1));
+        getContentPane().add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 140, 60, -1));
 
         jButton9.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton9.setText("5");
-        getContentPane().add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 100, 60, -1));
+        getContentPane().add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 140, 60, -1));
 
         jButton10.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton10.setText("6");
-        getContentPane().add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 100, 60, -1));
+        getContentPane().add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 140, 60, -1));
 
         jButton11.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton11.setText("7");
-        getContentPane().add(jButton11, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 140, 60, 40));
+        getContentPane().add(jButton11, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 180, 60, 40));
 
         jButton12.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton12.setText("8");
-        getContentPane().add(jButton12, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 140, 60, 40));
+        getContentPane().add(jButton12, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 180, 60, 40));
 
         jButton13.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jButton13.setText("9");
-        getContentPane().add(jButton13, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, 60, 40));
+        getContentPane().add(jButton13, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 180, 60, 40));
 
         jButton14.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
         jButton14.setText("10");
         jButton14.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(jButton14, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 140, 60, 40));
+        getContentPane().add(jButton14, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 180, 60, 40));
 
         jButton15.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
         jButton15.setText("11");
-        getContentPane().add(jButton15, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 140, -1, 40));
+        getContentPane().add(jButton15, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 180, 60, 40));
 
         jButton16.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
         jButton16.setText("12");
-        getContentPane().add(jButton16, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 140, -1, 40));
+        getContentPane().add(jButton16, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 180, 60, 40));
+
+        jButton17.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jButton17.setText("AM");
+        getContentPane().add(jButton17, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 60, 180, 40));
+
+        jButton18.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jButton18.setText("FM");
+        getContentPane().add(jButton18, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 180, 40));
+
+        jCheckBox1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jCheckBox1.setText("Guardar Estación");
+        getContentPane().add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 100, 360, 40));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -170,6 +184,8 @@ public class GUI extends javax.swing.JFrame {
     private javax.swing.JButton jButton14;
     private javax.swing.JButton jButton15;
     private javax.swing.JButton jButton16;
+    private javax.swing.JButton jButton17;
+    private javax.swing.JButton jButton18;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
@@ -178,6 +194,7 @@ public class GUI extends javax.swing.JFrame {
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
     private javax.swing.JButton jButton9;
+    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 
